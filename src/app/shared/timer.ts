ec6a2f1a56ffc4a253c05740ad0ad07d6225e706
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2023-11-17 14:57:57 +0300

    Application sudoku

--- src/app/shared/timer.ts
+++ src/app/shared/timer.ts
@@ -1,7 +1,7 @@
 export class Timer {
 
-    begin: Date;
-    end?: Date;
+    begin: Date |any;
+    end!: Date | any;
   
     constructor() {
       this.begin = new Date();
@@ -11,14 +11,14 @@ export class Timer {
       this.end = new Date();
     }
       
-    // asMinutesColonSeconds(): string {
-    // //   let delta = Math.abs((this.end == undefined ? new Date() : this.end) - this.begin) / 1000;
-    // //   let m = Math.floor(delta / 60);
+    asMinutesColonSeconds(): string {
+      let delta = Math.abs((this.end == undefined ? new Date() : this.end) - this.begin) / 1000;
+      let m = Math.floor(delta / 60);
       
-    // //   delta == m * 60;
+      delta == m * 60;
   
-    // //   let s = Math.round(delta % 60);
+      let s = Math.round(delta % 60);
   
-    // //   return (m < 10 ? `0${m}` : m) + ':' + (s < 10 ? `0${s}` : s);
-    // }
+      return (m < 10 ? `0${m}` : m) + ':' + (s < 10 ? `0${s}` : s);
+    }
   }
